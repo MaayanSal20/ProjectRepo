6a742bcd197f9a5f5bc87eb331984ec059dd685a
hala19 <hala.aboresh19@gmail.com>
2025-12-31 17:01:00 +0200

    update home page

--- G14_client/src/client_gui/HomePageController.java
+++ G14_client/src/client_gui/HomePageController.java
@@ -1,25 +1,65 @@
 package client_gui;
 
-import client.ClientUI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class HomePageController {
 
     @FXML
-    private void onRegisterSubscriberClick(ActionEvent event) {
-        System.out.println("Register Subscriber clicked");
+    private void onOrderTableClick(ActionEvent event) {
+        System.out.println("TODO: Order Table");
+    }
+
+    @FXML
+    private void onCancelReservationClick(ActionEvent event) {
+        System.out.println("TODO: Cancel Reservation");
+    }
+
+    @FXML
+    private void onRepAreaClick(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/client_gui/RepLogin.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("/client_gui/client.css").toExternalForm());
+
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Representative Login");
+            stage.show();
+
+        } catch (Exception e) {
+            System.out.println("Failed to load RepLogin.fxml: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onManagerAreaClick(ActionEvent event) {
+        System.out.println("TODO: Manager Area");
     }
 
     @FXML
     private void onLogoutClick(ActionEvent event) {
-        System.out.println("Logout clicked");
-
         try {
-            if (ClientUI.client != null) {
-                ClientUI.client.quit();
-            }
-        } catch (Exception ignored) {}
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/client_gui/ClientLogin.fxml"));
+            Parent root = loader.load();
 
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("/client_gui/client.css").toExternalForm());
+
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Client Login");
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
